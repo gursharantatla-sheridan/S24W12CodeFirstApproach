58ed698e52128e174465b1d38971f7eaf70ae2e2
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-30 08:43:02 -0400

    create entities

---
+++ S24W12CodeFirstApproach/Standard.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24W12CodeFirstApproach
+{
+    public class Standard
+    {
+        // scalar properties
+        public int StandardId { get; set; }
+        public string? Name { get; set; }
+
+        // navigation properties
+        public ICollection<Student>? Students { get; set; }
+    }
+}
---
+++ S24W12CodeFirstApproach/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S24W12CodeFirstApproach
+{
+    public class Student
+    {
+        // scalar properties
+        public int StudentId { get; set; }
+        public string? Name { get; set; }
+        public int? StandardId { get; set; }
+
+        // navigation properties
+        public Standard? Standard { get; set; }
+    }
+}
