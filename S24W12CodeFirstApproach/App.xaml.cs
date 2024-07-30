450c92f54def67f70895dfec97a991f0dc6c5fff
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2024-07-30 08:24:07 -0400

    Add project files.

---
+++ S24W12CodeFirstApproach/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace S24W12CodeFirstApproach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
